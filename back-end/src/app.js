69d9c7e8a2a1e4c42c37e059720081ce634b26e2
leamsigc <leamsigc@hotmail.com>
2020-11-22 18:52:32 +0100

    Get the prod working finally

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -1,7 +1,9 @@
 const app = require('express')();
 const PORT = 3000 || process.env.PORT;
 
-
+app.get('/', (req, res) => {
+     res.json('hello');
+});
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
 });
